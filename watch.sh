cbfd190cce4dc8073cf36519759b5c2652919235
Yevgheni Petkevich <Yevgheni.Petkevich@ziebel.com>
2015-11-11 15:55:44 +0100

    Fix watch script to watch everything in parallel.
    Fixes #31.

--- watch.sh
+++ watch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-coffee -o build/lib/ -cw src/timeline-scale-library.litcoffee
-coffee -o build/demo/ -cw src/timeline-scale-example.litcoffee
-jade -o build/demo/ -Pw src/demo.jade
+coffee -o build/lib/ -cw src/timeline-scale-library.litcoffee &
+coffee -o build/demo/ -cw src/timeline-scale-example.litcoffee &
+jade -o build/demo/ -Pw src/demo.jade &
